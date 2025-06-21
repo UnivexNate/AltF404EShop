75e0c4110298423a99cc44577a7902388599192e
Univex Nate <98638748+UnivexNate@users.noreply.github.com>
2025-06-21 16:32:40 -0400

    produitcontroller class

---
+++ catalogue/src/main/java/com/example/catalogue/controller/ProduitController.java
@@ -0,0 +1,44 @@
+package com.example.catalogue.controller;
+
+import com.example.catalogue.model.Produit;
+import com.example.catalogue.service.ProduitService;
+import com.example.catalogue.exception.ProduitNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produits")
+public class ProduitController {
+
+    @Autowired
+    private ProduitService produitService;
+
+    @GetMapping
+    public List<Produit> getAllProduits() {
+        return produitService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Produit getProduitById(@PathVariable Long id) {
+        return produitService.findById(id);
+    }
+
+    @PostMapping
+    public Produit createProduit(@RequestBody Produit produit) {
+        return produitService.save(produit);
+    }
+
+    @PutMapping("/{id}")
+    public Produit updateProduit(@PathVariable Long id, @RequestBody Produit produit) {
+        produit.setId(id);
+        return produitService.save(produit);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduit(@PathVariable Long id) {
+        produitService.delete(id);
+    }
+}
